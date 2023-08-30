3518c3cab4c1ba10bdc7fb235774b2dfd454b800
blankhall98 <blankhall@ciencias.unam.mx>
2023-08-30 06:17:57 +0200

    clase analisis

--- analysis.R
+++ analysis.R
@@ -15,7 +15,26 @@ important_columns <- c("Country Code",years)
 database <- poverty_database[,important_columns]
 names(database)[names(database) == "Country Code"] <- "country_code"
 
-code <- "ALB"
-sub <- database %>% filter(country_code == code)
-sub <- sub %>% select(where(~ !any(is.nan(.))))
-View(sub)
+filter_base <- function(df,code){
+  sub <- database %>% filter(country_code == code)
+  sub <- sub %>% select(where(~ !any(is.nan(.))))
+  
+  value_row <- sub %>% slice(1) %>% select(2:ncol(sub))
+  
+  value_vector <- as.vector(unlist(value_row))
+  
+  sub_df <- data.frame(
+    years <- years,
+    value <- value_vector
+  )
+  
+  p <- ggplot(sub_df, aes(x = years, y = value)) +
+    geom_line() +
+    geom_point() +
+    ggtitle(paste("Time Series Data for", code)) +
+    xlab("Year") +
+    ylab("Value")
+  
+  print(p)
+}
+
